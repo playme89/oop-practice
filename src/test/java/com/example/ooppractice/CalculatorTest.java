66ee7d7e5efa0282f49a2cf33d1faead03c78d2a
jaesung.ko <playme89@naver.com>
2022-08-28 18:54:45 +0900

    oop연습

---
+++ src/test/java/com/example/ooppractice/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.example.ooppractice;
+
+import com.example.ooppractice.calculate.PositiveNumber;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class CalculatorTest {
+
+
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    @DisplayName("덧셈 연산을 계산한다")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+        assertThat(calculateResult).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
+    }
+}
